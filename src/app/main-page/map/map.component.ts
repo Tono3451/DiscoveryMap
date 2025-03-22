97799755ddd9e4afc3a34839cc680f4cc10ed11b
Tono3451 <116306863+Tono3451@users.noreply.github.com>
2025-03-22 16:19:02 +0000

    solucionado problema de los bordes del mapa

--- src/app/main-page/map/map.component.ts
+++ src/app/main-page/map/map.component.ts
@@ -19,11 +19,18 @@ export class MapComponent implements OnInit {
 
   private initMap(): void {
     // Inicializar el mapa
-    this.map = L.map('map').setView([40.4168, -3.7038], 6); // Coordenadas de Madrid, España
+    this.map = L.map('map', {
+      zoomControl: false // Desactiva el control de zoom predeterminado
+    }).setView([40.4168, -3.7038], 6); // Coordenadas de Madrid, España
 
     // Añadir una capa de tiles (por ejemplo, OpenStreetMap)
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    // Mover el control de zoom a la esquina inferior derecha
+    L.control.zoom({
+      position: 'bottomright' // Puedes usar 'topleft', 'topright', 'bottomleft', o 'bottomright'
     }).addTo(this.map);
   }
 }
